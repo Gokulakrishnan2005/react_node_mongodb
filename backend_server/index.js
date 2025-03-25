8c9e0fb7282d1518d55089b40edf709ff304f9fd
Gokulakrishnan2005 <gokulakrishnanvgl05@gmail.com>
2025-03-25 07:39:46 +0530

    Connected with mongodb �

--- backend_server/index.js
+++ backend_server/index.js
@@ -1,15 +1,20 @@
 import express from 'express';
+import mongoose from'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+// const mongoURI = process.env.MONGODB_URI;
+
+mongoose
+    .connect(process.env.MONGODB)
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+
+
 
 const app = express();
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
-
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
-
-// app.get('/', (req, res) => {
-//     res.send('Hello, World!');
-// });
